a57697cce3233e648631038342608193b55ed285
Marcel Pociot <m.pociot@gmail.com>
2023-03-07 14:13:40 +0100

    wip

--- config/horizon.php
+++ config/horizon.php
@@ -189,7 +189,7 @@ return [
             'maxJobs' => 0,
             'memory' => 128,
             'tries' => 1,
-            'timeout' => 60,
+            'timeout' => 3600,
             'nice' => 0,
         ],
     ],
