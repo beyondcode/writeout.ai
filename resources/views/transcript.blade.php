0e7c79ae1e47686e9fc7067cd882fcc397d5b24c
Marcel Pociot <m.pociot@gmail.com>
2023-03-07 14:14:13 +0100

    wip

--- resources/views/transcript.blade.php
+++ resources/views/transcript.blade.php
@@ -41,25 +41,26 @@
                         We're translating your transcript. This may take some time.<br />
                         The page will refresh automatically when the translation is complete.
                     </p>
+                @else
+                    <div class="mt-12 text-lg tracking-tight text-slate-700">
+                        <form method="post" action="{{ action(\App\Http\Controllers\TranslateTranscriptController::class, $transcript) }}" class="flex items-center space-x-4 justify-center">
+                            @csrf
+                            <label for="language" class="block text-sm font-medium leading-6 text-gray-900">Translate to: </label>
+                            <select id="language" name="language" class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <option selected value="">Original Language</option>
+                                @foreach(config('writeout.translatable_languages') as $language)
+                                    <option value="{{ $language }}" @if(request()->get('language') === $language) selected @endif>
+                                        {{ $language }} @if($transcript->translations[$language] ?? false) (translated) @endif
+                                    </option>
+                                @endforeach
+                            </select>
+                            <button type="submit" class="group inline-flex items-center justify-center rounded-md py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600"
+                               href="/transcribe">
+                                <span>Change</span>
+                            </button>
+                        </form>
+                    </div>
                 @endif
-                <div class="mt-12 text-lg tracking-tight text-slate-700">
-                    <form method="post" action="{{ action(\App\Http\Controllers\TranslateTranscriptController::class, $transcript) }}" class="flex items-center space-x-4 justify-center">
-                        @csrf
-                        <label for="language" class="block text-sm font-medium leading-6 text-gray-900">Translate to: </label>
-                        <select id="language" name="language" class="block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            <option selected value="">Original Language</option>
-                            @foreach(config('writeout.translatable_languages') as $language)
-                                <option value="{{ $language }}" @if(request()->get('language') === $language) selected @endif>
-                                    {{ $language }} @if($transcript->translations[$language] ?? false) (translated) @endif
-                                </option>
-                            @endforeach
-                        </select>
-                        <button type="submit" class="group inline-flex items-center justify-center rounded-md py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600"
-                           href="/transcribe">
-                            <span>Change</span>
-                        </button>
-                    </form>
-                </div>
                 <div class="mt-12 text-lg tracking-tight text-slate-700">
                     <a class="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-600 text-white hover:text-slate-100 hover:bg-gray-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600"
                        href="{{ action(\App\Http\Controllers\DownloadTranscriptController::class, [
